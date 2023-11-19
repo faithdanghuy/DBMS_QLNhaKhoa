ecb2b2a720614be52f90053774b6033e55688b2e
kru01 <79639749+kru01@users.noreply.github.com>
2023-11-19 22:22:41 +0700

    SQL update - wrote insert stProc and populated data for LICHHEN - wrote login stProc as misc - wrote batch script to initiate the database

---
+++ SQL/setupDB.bat
@@ -0,0 +1,18 @@
+
+@echo off
+cls
+chcp 65001
+
+@echo ---- Creating Nhom3_QLNhaKhoa...
+SQLCMD -E -dmaster -f65001 -i".\createDB.sql"
+
+@echo ---- Creating Stored Procedures...
+for %%G in (.\storedProcs\*.sql) do SQLCMD -E -dmaster -f65001 -i"%%G"
+
+@echo ---- Populating Data...
+SQLCMD -E -dmaster -f65001 -i".\data.sql"
+
+@echo ---- Granting Permissions...
+SQLCMD -E -dmaster -f65001 -i".\permissions.sql"
+
+PAUSE
